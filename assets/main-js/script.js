94eeb5d59f38ace92dc059d6fe2bebaa931a0198
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-06 18:21:18 +0200

    Add data acquisition section
    Add three prompts for each data input
    
    Add related variables to the prompts

--- assets/main-js/script.js
+++ assets/main-js/script.js
@@ -1,7 +1,18 @@
 
 /*INTRODUZIONE PER L'UTENTE*/
-alert(`Salve, benvenuto a bordo!
-       seguite le istruzioni per compilare il biglietto`)
-//-alert- mi permette di stampare semplici messaggi a schermo//
 
+alert(`Salve, benvenuto a bordo!
+seguite le istruzioni per compilare il biglietto`)
+//-alert mi permette di stampare semplici messaggi a schermo//
+
+/*ACQUISIZIONE DATI*/
+
+let full_name = prompt("Inserire nome e cognome separati da uno spazio")
+//dichiaro e ,tramite -prompt, assegno valore alla variabile -full_name(inizializzata all'atto dell'assegnazione)
+
+let eta = prompt("inserire età")
+//dichiaro e, tramite -prompt, assegno valore alla varibabile -eta(inizializzata all'atto dell'assegnazione)
+
+let lughezza_tratta = prompt(`Inserire la lunghezza, in km, della tratta che si desidera percorrere`)
+//dichiaro e, tramite -prompt, assegno valore alla variabille -eta(inizializzata all'atto dell'assegnazione)
 
