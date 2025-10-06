0450f067fdb546d446897142f45333e088f37b9d
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-06 18:56:50 +0200

    Add "Conditions for managing the discount" section
    Declaration -discounted_price outside of the conditions
    
    Add -if for discount for minors
    
    Add -if for discount for over 70s

--- assets/main-js/script.js
+++ assets/main-js/script.js
@@ -1,9 +1,9 @@
-
 /*INTRODUZIONE PER L'UTENTE*/
 
 alert(`Salve, benvenuto a bordo!
 seguite le istruzioni per compilare il biglietto`)
 //-alert mi permette di stampare semplici messaggi a schermo//
+
 
 /*ACQUISIZIONE DATI*/
 
@@ -16,6 +16,7 @@ let eta = Number(prompt("inserire età"))
 let lunghezza_tratta = Number(prompt(`Inserire la lunghezza, in km, della tratta che si desidera percorrere`))
 //dichiaro e, tramite -prompt, assegno valore alla variabille -eta(inizializzata all'atto dell'assegnazione)
 
+
 /*CALCOLO PREZZO PIENO*/
 
 const prezzo_km = 0.21
@@ -23,6 +24,29 @@ const prezzo_km = 0.21
 
 let prezzo_pieno = prezzo_km * lunghezza_tratta
 //calcolo il prezzo pieno della tratta, lo userò poi con eventuali sconti
+
+
+/*CONDIZIONI PER GESTIRE LO SCONTO*/
+
+let prezzo_scontato
+//-prezzo-scontato è necessaria per poter trasportare fuori dalle condizioni il valore calcolato
+
+if(eta <= 18){
+//controllo se l'utente è minorenne, applicando nel caso lo sconto
+
+    prezzo_scontato = prezzo_pieno * (1 - 0.2)  
+
+} else if(eta >= 70){
+//controntrollo se l'untente è over 70(compreso), applicando nel caso lo sconto
+
+    prezzo_scontato = prezzo_pieno * (1 - 0.4)
+
+}
+
+
+
+
+
 
 
 
