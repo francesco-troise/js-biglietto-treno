febcf5b819f5c458c60b3510695ad64c39d1c478
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-06 18:12:11 +0200

    add introductory section for user

--- assets/main-js/script.js
+++ assets/main-js/script.js
@@ -1,1 +1,7 @@
 
+/*INTRODUZIONE PER L'UTENTE*/
+alert(`Salve, benvenuto a bordo!
+       seguite le istruzioni per compilare il biglietto`)
+//-alert- mi permette di stampare semplici messaggi a schermo//
+
+
