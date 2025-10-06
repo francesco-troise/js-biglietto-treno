9db5524f54e6f7019f5893172fbcb8033898c27c
FrancescoTroise <francesco.troise98@gmail.com>
2025-10-06 18:35:27 +0200

    aggiungi sezione sconto
    aggiungi una constante -prezzo_km
    
    calcola -prezzo_pieno

--- assets/main-js/script.js
+++ assets/main-js/script.js
@@ -10,9 +10,20 @@ seguite le istruzioni per compilare il biglietto`)
 let full_name = prompt("Inserire nome e cognome separati da uno spazio")
 //dichiaro e ,tramite -prompt, assegno valore alla variabile -full_name(inizializzata all'atto dell'assegnazione)
 
-let eta = prompt("inserire età")
+let eta = Number(prompt("inserire età"))
 //dichiaro e, tramite -prompt, assegno valore alla varibabile -eta(inizializzata all'atto dell'assegnazione)
 
-let lughezza_tratta = prompt(`Inserire la lunghezza, in km, della tratta che si desidera percorrere`)
+let lunghezza_tratta = Number(prompt(`Inserire la lunghezza, in km, della tratta che si desidera percorrere`))
 //dichiaro e, tramite -prompt, assegno valore alla variabille -eta(inizializzata all'atto dell'assegnazione)
+
+/*CALCOLO PREZZO PIENO*/
+
+const prezzo_km = 0.21
+//dichiaro ed inizilizzo la constante(-prezzo_km è il prezzo al km della tratta)
+
+let prezzo_pieno = prezzo_km * lunghezza_tratta
+//calcolo il prezzo pieno della tratta, lo userò poi con eventuali sconti
+
+
+
 
